6f8f93defda5d507aa94e13f0761773ccd94a40b
sushantsinghrathore <ssr7705@gmail.com>
2025-08-17 06:24:22 +0530

    add MainActivity with Compose setup and navigation host

--- app/src/main/java/com/ssr/animexplore/MainActivity.kt
+++ app/src/main/java/com/ssr/animexplore/MainActivity.kt
@@ -3,45 +3,40 @@ package com.ssr.animexplore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ssr.animexplore.ui.theme.AnimeXploreTheme
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.ssr.animexplore.ui.animedetail.AnimeDetailScreen
+import com.ssr.animexplore.ui.animelist.AnimeListScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            AnimeXploreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = "anime_list"
+            ) {
+                composable("anime_list") {
+                    AnimeListScreen { animeId ->
+                        navController.navigate("animeDetail/$animeId")
+                    }
                 }
+
+                composable(
+                    route = "animeDetail/{animeId}",
+                    arguments = listOf(navArgument("animeId") { type = NavType.IntType })
+                ) { backStackEntry ->
+                    val animeId = backStackEntry.arguments?.getInt("animeId") ?: 0
+                    AnimeDetailScreen()
+                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimeXploreTheme {
-        Greeting("Android")
     }
 }
